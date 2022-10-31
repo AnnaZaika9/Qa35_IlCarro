9e88495874a707e8205f05cc53b51fe01d60dbc2
AnnaZaika9 <a.horujaya@gmail.com>
2022-10-31 19:10:09 +0200

    last

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -7,14 +7,14 @@ public class SearchTests extends TestBase{
 
 
 
-    @Test(groups = {"smoke"})
+    @Test
     public void searchCurrentMonthSuccess(){
         app.getSearch().searchCurrentMonth2("Tel Aviv","10/28/2022","10/30/2022");
         app.getSearch().submit();
         Assert.assertTrue(app.getSearch().isListOfCarsAppeared());
         logger.info("In the assertion, it is checked whether there were cars for the current month");
     }
-    @Test
+    @Test(groups = {"smoke"})
     public void searchNextMonthSuccess(){
         app.getSearch().searchNextMonth("Jerusalem Israel","11/25/2022","11/30/2022");
         app.getSearch().submit();
